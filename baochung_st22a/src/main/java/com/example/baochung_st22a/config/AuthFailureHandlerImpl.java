ef9da54664d752677e8d05ff1c6590e46ebb86e5
ChungCr7 <baochungas3@gmail.com>
2024-12-06 15:04:29 +0700

    cập nhật fix lỗi

--- baochung_st22a/src/main/java/com/example/baochung_st22a/config/AuthFailureHandlerImpl.java
+++ baochung_st22a/src/main/java/com/example/baochung_st22a/config/AuthFailureHandlerImpl.java
@@ -44,22 +44,22 @@ public class AuthFailureHandlerImpl extends SimpleUrlAuthenticationFailureHandle
 						userService.increaseFailedAttempt(userDtls);
 					} else {
 						userService.userAccountLock(userDtls);
-						exception = new LockedException("Your account is locked !! failed attempt 3");
+						exception = new LockedException("Tài khoản của bạn đã bị tạm khóa 5 phút, do nhập sai mật khẩu quá 5 lần! Vui lòng thử lại sau!");
 					}
 				} else {
 
 					if (userService.unlockAccountTimeExpired(userDtls)) {
-						exception = new LockedException("Your account is unlocked !! Please try to login");
+						exception = new LockedException("Tài khoản của bạn đã được mở khóa !! Vui lòng thử đăng nhập");
 					} else {
-						exception = new LockedException("your account is Locked !! Please try after sometimes");
+						exception = new LockedException("Tài khoản của bạn đã bị khóa!! Vui lòng thử đăng nhập lại");
 					}
 				}
 
 			} else {
-				exception = new LockedException("your account is inactive");
+				exception = new LockedException("Tài khoản của bạn không hoạt động");
 			}
 		} else {
-			exception = new LockedException("Email & password invalid");
+			exception = new LockedException("Email hoặc mật khẩu không hợp lệ");
 		}
 
 		super.setDefaultFailureUrl("/signin?error");
